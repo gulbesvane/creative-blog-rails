df3296c0871d8e44508c527e2cc82701f24ec399
Ruta <gulbesvane@gmail.com>
2022-05-11 20:18:17 +0100

    removed create from contacts controller

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,14 +2,12 @@ class Contact < MailForm::Base
     attribute :name,      validate: true
     attribute :email,     validate: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
     attribute :message,    validate: true
-    attribute :nickname,  captcha: true
   
-    # Declare the e-mail headers. It accepts anything the mail method
-    # in ActionMailer accepts.
+  
     def headers
       {
         :subject => "Contact Form Inquiry",
-        :to => "gulbesvane@gmail.com",
+        :to => "gulbesvane@inbox.lv",
         :from => %("#{name}" <#{email}>)
       }
     end
--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,7 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
+# cancel rails error field being added
 ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
     html_tag.html_safe
 end
