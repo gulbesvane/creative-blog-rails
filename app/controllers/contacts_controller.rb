1bc953ecbb3685a05e096b8762369ba75d05df02
Ruta <gulbesvane@gmail.com>
2022-05-12 12:35:57 +0100

    tried some more changes to make contact work

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,14 +3,14 @@ class ContactsController < ApplicationController
         @contact = Contact.new
       end
     
-    #   def create
-    #     @contact = Contact.new(params[:contact])
-    #     @contact.request = request
-    #     if @contact.deliver
-    #       flash.now[:success] = 'Message sent!'
-    #     else
-    #       flash.now[:error] = 'Could not send message'
-    #       render :index
-    #     end
-    #   end
+      def create
+        @contact = Contact.new(params[:contact])
+        @contact.request = request
+        if @contact.deliver
+          flash.now[:success] = 'Message sent!'
+        else
+          flash.now[:error] = 'Could not send message'
+          render :index
+        end
+      end
 end
