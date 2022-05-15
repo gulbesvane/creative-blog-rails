3de1577295c5de89e8320f0c2f34e1d18f7f7f36
Ruta <gulbesvane@gmail.com>
2022-05-15 20:24:55 +0100

    last changes to contacts form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,21 +1,21 @@
 class ContactsController < ApplicationController
   require 'mail_form'
     def index
-        
-      end
-          @contact = Contact.new
-      def new
-
-      end
+      @contact = Contact.new
+    end
+          
+    def new
+      @contact = Contact.new
+    end
     
-      def create
-        @contact = Contact.new(params[:contact])
-        @contact.request = request
-        if @contact.deliver
-          flash.now[:success] = 'Message sent, thank you!'
-        else
-          flash.now[:error] = 'Could not send message'
-          render :index
-        end
+    def create
+      @contact = Contact.new(params[:contact])
+      @contact.request = request
+      if @contact.deliver
+        flash.now[:success] = 'Message sent, thank you!'
+      else
+        flash.now[:error] = 'Could not send message'
+        render :index
       end
+    end
 end
